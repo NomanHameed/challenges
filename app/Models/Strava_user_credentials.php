262ff5819f9a7b459f41605ab27d38620800c019
Haider Ali <se.haiderali@gmail.com>
2022-07-30 05:35:34 +0500

    added strava singleton client

--- app/Models/Strava_user_credentials.php
+++ app/Models/Strava_user_credentials.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use CodeToad\Strava\Strava;
+use GuzzleHttp\Client;
 use Illuminate\Database\Eloquent\Model;
 
 class Strava_user_credentials extends Model
 {
-    //
+    public static $http_client = null;
+
+    public function httpClient(): ?Strava
+    {
+        if(!self::$http_client) {
+
+            self::$http_client = new Strava(
+                config('ct_strava.client_id'),
+                config('ct_strava.client_secret'),
+                config('ct_strava.redirect_uri'),
+                new Client()
+            );
+        }
+        return self::$http_client;
+
+
+    }
+
 }
